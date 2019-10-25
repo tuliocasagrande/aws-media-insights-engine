b3ae0a940fe7a89c176f0f2f29f9e92cde5bcde9
ianwow <ianwow@amazon.com>
2019-10-24 17:21:34 -0700

    enable subresource integrity checks so browsers can verify the JS and CSS files they fetch from CDNs are delivered without manipulation

--- webapp/vue.config.js
+++ webapp/vue.config.js
@@ -1,4 +1,17 @@
+var SriPlugin = require('webpack-subresource-integrity');
+
 module.exports = {
+  configureWebpack: {
+    output: {
+      crossOriginLoading: 'anonymous',
+    },
+    plugins: [
+      new SriPlugin({
+        hashFuncNames: ['sha256', 'sha384'],
+        enabled: true
+      }),
+    ]
+  },
   devServer: {
     https: false
   }
